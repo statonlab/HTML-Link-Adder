233fc42cab93fb0df4a0cef90df63dc5c3423710
Abdullah Almsaeed <almasaeed2010@gmail.com>
2020-05-07 14:21:25 -0400

    add deployment code

---
+++ deply.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git remote add origin https://github.com/statonlab/statonlab.github.io.git
+git push -f origin master
+
+cd -
